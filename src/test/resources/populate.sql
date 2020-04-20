4b5a74ec5cb310f581bda499730774ad47494376
Kostyantyn Gromov <kostyantyn_gromov@comcast.com>
2020-04-20 19:57:39 +0300

    finish tests

--- src/test/resources/populate.sql
+++ src/test/resources/populate.sql
@@ -1,1 +1,1 @@
-INSERT INTO user (id, user_name, password) VALUES (111, 'John Doe', '$2a$08$.7neC4bHa91NDbIHAq.2WOi1WWDNwJoXGJbNjEegqjYaw51K4CrJm');
+INSERT INTO user (id, user_name, password) VALUES (111, 'admin', '$2a$08$ABleJ7tauL5Vht.aT34ZFezB0.rULTO25AgrN3qhz3pjkv.L/ei76');
